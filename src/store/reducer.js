83cbf4f62d1b2fe4275566c7543a80cbeed11027
Josipa Dadić <josipa.dadic@gmail.com>
2020-11-27 20:44:18 +0100

    assignment finished

---
+++ src/store/reducer.js
@@ -0,0 +1,29 @@
+const initialState = {
+  persons: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_PERSON":
+      const newPerson = {
+        id: Math.random(),
+        name: "Max",
+        age: Math.floor(Math.random() * 40),
+      };
+      return {
+        ...state,
+        persons: state.persons.concat(newPerson),
+      };
+
+    case "DELETE_PERSON":
+      return {
+        ...state,
+        persons: state.persons.filter(
+          (person) => person.id !== action.personId
+        ),
+      };
+  }
+  return state;
+};
+
+export default reducer;
